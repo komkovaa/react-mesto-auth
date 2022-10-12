db40208995fe923831a34d439ecf3a2372544086
komkova <komkova.ar@gmail.com>
2022-10-12 15:51:32 +0300

    Refactor: объявить переменную через const, скрыть верстку через null

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,10 @@
 function Footer(props) {
-    let date = new Date().getFullYear();
+    const date = new Date().getFullYear();
     return (
         <footer className="footer">
             {props.loggedIn ? (
         <p className="footer__author">&copy; {date} Анастасия Комкова</p>
-      ) : (
-        <p className="footer__author" style={{display: 'none'}}>&copy; {date} Анастасия Комкова</p>
-      )
-      }
+      ) : null}
         </footer>
     )
 }
